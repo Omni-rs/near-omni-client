6ac77e8880bb64c14685cb44450c8e2a2117db0e
edsonalcala <edson_alcala@msn.com>
2025-05-19 16:21:44 +0100

    module export and format

--- src/near_omni_client/crypto/__init__.py
+++ src/near_omni_client/crypto/__init__.py
@@ -1,4 +1,5 @@
 from .public_key import PublicKey
 from .constants import KeyType
+from .keypair_ed25519 import KeyPairEd25519
 
-__all__ = ["PublicKey", "KeyType"]
+__all__ = ["PublicKey", "KeyType", "KeyPairEd25519"]
