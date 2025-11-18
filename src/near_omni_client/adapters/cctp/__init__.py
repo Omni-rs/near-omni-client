3ecf658aaa7a149dc583d5e3c91c065b9cda40b0
edsonalcala <edson_alcala@msn.com>
2025-11-18 20:14:47 +0100

    lint fixed

--- src/near_omni_client/adapters/cctp/__init__.py
+++ src/near_omni_client/adapters/cctp/__init__.py
@@ -1,7 +1,13 @@
 from .attestation_service import AttestationService
+from .fee_service import FeeService
 from .messenger_contract import MessengerContract
 from .transmitter_contract import TransmitterContract
 from .usdc_contract import USDCContract
-from .fee_service import FeeService
 
-__all__ = ["AttestationService", "MessengerContract", "TransmitterContract", "USDCContract", "FeeService"]
+__all__ = [
+    "AttestationService",
+    "FeeService",
+    "MessengerContract",
+    "TransmitterContract",
+    "USDCContract",
+]
